86f126aa3881bf12b933180131e3838ef90ecc20
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2023-07-05 12:03:43 -0400

    Init commot

---
+++ index.js
@@ -0,0 +1,154 @@
+const contentCards = document.querySelector('.content-cards');
+const select = document.getElementById('filter-by');
+const search = document.getElementById('search');
+const filterStatus = document.getElementById('status-filter');
+const filterGender = document.getElementById('gender-filter');
+const modal = document.querySelector('.modal');
+const listSearch = document.querySelector('.listSearch');
+
+let query = 'name';
+let parameters = '';
+
+
+function showStatusGender(query){
+    search.setAttribute('disabled','true');
+    if (query == 'status') {
+        filterStatus.style.display = 'flex';
+        filterGender.style.display = 'none';
+    }else{
+        filterStatus.style.display = 'none';
+        filterGender.style.display = 'flex';
+    }
+}
+
+function ocultarStatusGender(){
+    search.removeAttribute('disabled');
+    filterStatus.style.display = 'none';
+    filterGender.style.display = 'none';
+}
+
+async function fetchCharacter(query,parameters,callback){
+    try {
+        const result = await     fetch(`https://rickandmortyapi.com/api/character?${query}=${parameters}`)
+        const data = await result.json();
+        callback(data.results);
+    } catch (error) {
+        console.log('nada');
+    }
+}
+
+const showData=(result)=>{
+    contentCards.innerHTML = '';
+    for (let i = 0; i < result.length; i++) {
+        const data = result[i];
+        const cards = document.createElement('div');
+        cards.classList.add('cards-personajes');
+        cards.innerHTML = `        
+        <div class="contain-img">
+        <img src="${data.image}" alt="" class="img-personaje">
+        </div>
+        <div class="contain-dataPersonaje">
+        <h3 class="name">
+        ${data.name}<br>
+        <span class="status-species">${data.status} - ${data.species}</span>
+        </h3>
+        <div class="last-location">
+        <span class="first-last-ubication">ultima ubicaion conocida:</span><br>
+        ${data.location.name}
+        </div>
+        <div class="first-aparicion">
+        <span class="first-last-ubication">Visto por primera vez en:</span><br>
+                ${data.name}
+            </div>
+        </div>`
+        cards.addEventListener('click',()=>{viewCharacter(data)})
+        contentCards.appendChild(cards)
+    }
+}
+        
+
+function viewCharacter(data){
+    document.body.style.overflow = 'hidden'
+    modal.style.display = 'flex';
+    modal.innerHTML = `        
+    <div class="contain-img">
+    <img src="${data.image}" alt="" class="img-personaje">
+    </div>
+    <div class="contain-dataPersonaje">
+    <h3 class="name">
+    ${data.name}<br>
+    <span class="status-species">${data.status} - ${data.species}</span>
+    </h3>
+    <div class="last-location">
+    <span class="first-last-ubication">ultima ubicaion conocida:</span><br>
+    ${data.location.name}
+    </div>
+    <div class="first-aparicion">
+    <span class="first-last-ubication">Visto por primera vez en:</span><br>
+            ${data.name}
+        </div>
+    </div>
+    <button class="exit">Volver</button>`
+
+    document.querySelector('.exit').addEventListener('click',()=>{
+        document.body.style.overflow = 'auto'
+        modal.style.display = 'none';
+        modal.innerHTML = '';
+    })
+
+}
+
+function showAutoComplete(result){
+    listSearch.innerHTML = '';
+    for (let i = 0; i < result.length; i++) {
+        const data = result[i];
+        const cards = document.createElement('div');
+        cards.classList.add('item-search');
+        cards.innerHTML = `        
+        <div class="contain-img">
+        <img src="${data.image}" alt="" class="img-personaje">
+        </div>
+        <div class="contain-dataPersonaje">
+        <h3 class="name">
+        ${data.name}<br>
+        <span class="status-species">${data.status} - ${data.species}</span>
+        </h3>
+        <div class="last-location">
+        <span class="first-last-ubication">ultima ubicaion conocida:</span><br>
+        ${data.location.name}
+        </div>
+        <div class="first-aparicion">
+        <span class="first-last-ubication">Visto por primera vez en:</span><br>
+                ${data.name}
+            </div>
+        </div>`
+        cards.addEventListener('click',()=>{viewCharacter(data)})
+        listSearch.appendChild(cards);
+    }
+}
+
+select.addEventListener('change',()=>{
+    listSearch.innerHTML = '';
+    search.value = '';
+    contentCards.innerHTML = '';
+    query = select.value;
+    if (query === 'status' || query === 'gender') {
+        showStatusGender(query);
+    }else{
+        ocultarStatusGender();
+    }
+});
+
+search.addEventListener('keydown',(e)=>{
+    fetchCharacter(query,search.value,showAutoComplete)
+});
+
+filterStatus.addEventListener('change',()=>{
+    parameters = filterStatus.value;
+    fetchCharacter(query,parameters,showData)
+});
+
+filterGender.addEventListener('change',()=>{
+    parameters = filterGender.value;
+    fetchCharacter(query,parameters,showData)
+});
