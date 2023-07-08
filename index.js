2d5ff07690c8021e851037caa7de907cf22d5de4
miguel-124C <121363772+miguel-124C@users.noreply.github.com>
2023-07-07 21:37:01 -0400

    Conclusion del proyecto

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const filterGender = document.getElementById('gender-filter');
 const modal = document.querySelector('.modal');
 const listSearch = document.querySelector('.listSearch');
 
+let urlPath = 'https://rickandmortyapi.com/api'
 let query = 'name';
 let parameters = '';
 
@@ -33,11 +34,15 @@ async function fetchCharacter(query,parameters,callback){
             contentCards.innerHTML = '';
             return;
         }
-        const result = await     fetch(`https://rickandmortyapi.com/api/character?${query}=${parameters}`)
+        const result = await     fetch(`${urlPath}/character?${query}=${parameters}`)
         const data = await result.json();
         callback(data.results);
     } catch (error) {
-        console.log('nada');
+        contentCards.innerHTML = '';
+        const message = document.createElement('div');
+        message.classList.add('error');
+        message.textContent = 'No se encuentra el personaje';
+        contentCards.appendChild(message);
     }
 }
 
@@ -54,15 +59,16 @@ const showData=(result)=>{
         <div class="contain-dataPersonaje">
         <h3 class="name">
         ${data.name}<br>
-        <span class="status-species">${data.status} - ${data.species}</span>
+            <span class="status-species">${(data.status == 'Alive')? 'Vivo': (data.status == 'Dead')? 'Muerto' : 'Desconocido'} - ${data.species}</span>
+            <span class="${(data.status == 'Alive')? 'alive': (data.status == 'Dead')? 'dead' : 'unknown'}"></span>
         </h3>
         <div class="last-location">
         <span class="first-last-ubication">ultima ubicaion conocida:</span><br>
         ${data.location.name}
         </div>
         <div class="first-aparicion">
-        <span class="first-last-ubication">Visto por primera vez en:</span><br>
-                ${data.name}
+        <span class="genero">Genero:</span><br>
+                ${data.gender}
             </div>
         </div>`
         cards.addEventListener('click',()=>{viewCharacter(data)})
@@ -88,8 +94,8 @@ function viewCharacter(data){
     ${data.location.name}
     </div>
     <div class="first-aparicion">
-    <span class="first-last-ubication">Visto por primera vez en:</span><br>
-            ${data.name}
+    <span class="genero">Genero:</span><br>
+            ${data.gender}
         </div>
     </div>
     <button class="exit">Volver</button>`
@@ -117,13 +123,13 @@ function showAutoComplete(result){
                 ${data.name}
                 <span>${data.status} - ${data.species}</span>
             </h3>
-            <div class="last-location-list">
-                <span>ultima ubicaion conocida:</span><br>
+            <div>
+                <span class="last-location-list">Ultima ubicaion conocida:</span><br>
                 ${data.location.name}
             </div>
-            <div class="first-aparicion-list">
-                <span class="first-last-ubication-list">Visto por primera vez en:</span><br>
-                    ${data.name}
+            <div>
+                <span class="genero">Genero:</span><br>
+                    ${data.gender}
             </div>
         </div>`
         cards.addEventListener('click',()=>{viewCharacter(data)})
